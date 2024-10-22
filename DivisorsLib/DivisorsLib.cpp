640dc6e63436e33bc7cbacd0b5125b2fdb7d916f
ergulerim <cgjpmoe6743_aehit@students.vizja.pl>
2024-10-22 19:39:36 +0200

    improved algorithm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,21 @@
 #include <vector>
-#include "Divisors.h"
+#include <cmath>
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
 	std::vector<unsigned long long> result;
+	unsigned long long limit = static_cast<unsigned long long>(sqrt(number));
+
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
 		if (number % i == 0)
+		{
 			result.push_back(i);
+			if (i != number / i)
+			{
+				result.push_back(number / i);
+			}
+		}
 	}
 	return result;
 }
